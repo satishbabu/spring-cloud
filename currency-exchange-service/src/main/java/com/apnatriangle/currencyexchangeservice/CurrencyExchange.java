556938ed640ca1846c93793f7ae602370ab33c8a
satishbabu <reachsatish@yahoo.com>
2021-08-14 17:17:30 -0400

    Added currency exchange service

--- currency-exchange-service/src/main/java/com/apnatriangle/currencyexchangeservice/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/apnatriangle/currencyexchangeservice/CurrencyExchange.java
@@ -1,2 +1,21 @@
-package com.apnatriangle.currencyexchangeservice;public class CurrencyExchange {
+package com.apnatriangle.currencyexchangeservice;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CurrencyExchange {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+
 }
