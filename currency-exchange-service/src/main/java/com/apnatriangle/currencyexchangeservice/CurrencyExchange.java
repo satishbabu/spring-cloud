96e247745f60fb0e8e682d95a0c5a42226162c35
satishbabu <reachsatish@yahoo.com>
2021-08-14 22:36:43 -0400

    added persisting using jpa

--- currency-exchange-service/src/main/java/com/apnatriangle/currencyexchangeservice/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/apnatriangle/currencyexchangeservice/CurrencyExchange.java
@@ -6,16 +6,25 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class CurrencyExchange {
+    @Id
     private Long id;
+
+    @Column(name = "currency_from")
     private String from;
+
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
-
+    private String environment;
 }
--- currency-exchange-service/src/main/java/com/apnatriangle/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/apnatriangle/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.apnatriangle.currencyexchangeservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +10,20 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
+    @Autowired
+    private Environment enviornment;
+
 
     @GetMapping("currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable String from,
             @PathVariable String to) {
-        return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        return new CurrencyExchange(
+                1000L,
+                from,
+                to,
+                BigDecimal.valueOf(50),
+                enviornment.getProperty("local.server.port"));
     }
 
 }
