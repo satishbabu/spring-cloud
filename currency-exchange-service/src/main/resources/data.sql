8cc8ad5ca383fe302d0f79db0952cc44c9d08f63
satishbabu <reachsatish@yahoo.com>
2021-08-14 22:36:27 -0400

    Add persistence using jpa

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into CURRENCY_EXCHANGE
+(id, currency_from, currency_to, conversion_multiple, environment)
+values (10001,'USD', 'INR', 75, '');
+insert into CURRENCY_EXCHANGE
+(id, currency_from, currency_to, conversion_multiple, environment)
+values (10002,'EUR', 'INR', 85, '');
+insert into CURRENCY_EXCHANGE
+(id, currency_from, currency_to, conversion_multiple, environment)
+values (10003,'AUD', 'INR', 25, '');
