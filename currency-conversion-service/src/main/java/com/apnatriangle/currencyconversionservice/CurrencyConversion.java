d003bd0d3f60b7221743ed354e6f607388b33b56
satishbabu <reachsatish@yahoo.com>
2021-08-15 07:37:33 -0400

    Added currency conversion

--- currency-conversion-service/src/main/java/com/apnatriangle/currencyconversionservice/CurrencyConversion.java
+++ currency-conversion-service/src/main/java/com/apnatriangle/currencyconversionservice/CurrencyConversion.java
@@ -1,2 +1,23 @@
-package com.apnatriangle.currencyconversionservice;public class CurrencyConversion {
+package com.apnatriangle.currencyconversionservice;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class CurrencyConversion {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal quantity;
+    private BigDecimal conversionMultiple;
+    private BigDecimal totalCalculatedAmount;
+    private String environment;
 }
