ca44803c8083e17b15e07c51e2bcae68679d5be1
satishbabu <reachsatish@yahoo.com>
2021-08-17 17:47:20 -0400

    Trying resilience and retry

---
+++ currency-exchange-service/src/main/java/com/apnatriangle/currencyexchangeservice/CiruitBreakerController.java
@@ -0,0 +1,25 @@
+package com.apnatriangle.currencyexchangeservice;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class CiruitBreakerController {
+
+    @GetMapping("/sample-api")
+    @Retry(name="sample-api")
+    public String sampleApi() {
+        log.info("In sampleApi()");
+
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity(
+                "http://locahost:8080/some-dummy-url",
+                String.class);
+        return forEntity.getBody();
+    }
+
+}
